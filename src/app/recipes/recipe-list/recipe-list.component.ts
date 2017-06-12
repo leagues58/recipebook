68c6826419b0446dc1963bcd7d2eb4ec676c2fab
Eric Reeves <leagues58@gmail.com>
2017-06-11 21:32:05 -0400

    Added body to recipe-list component.  Recipes added in code now show in list.

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+
+    new Recipe('Spaghetti', 'Classic Italian dish', 'https://budgetbytes.com/wp-content/uploads/2015/11/Spaghetti-with-Vegetable-and-Meat-Sauce-bowl-front.jpg'),
+    new Recipe('Chicken Carbonara', 'Bastardized Italian dish', 'http://www.publicdomainpictures.net/pictures/210000/velka/spaghetti-carbonara-14896856952Hb.jpg')
+
+  ];
+
 
   constructor() { }
 
